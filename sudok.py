b8dbd5a7916470dcc1ba49e7a8fd93c2002c9608
Rodri.inc <faguezp@gmail.com>
2020-04-22 19:16:45 -0300

    now can verify if the selected position is possible

--- sudok.py
+++ sudok.py
@@ -3,14 +3,21 @@ r = ["A", "B", "C", "D"]
 board = [
     ["A", "B", "C", "D"],
     ["", "", "", ""],
-    ["", "", "", ""],
+    ["", "", "B", ""],
     ["", "", "", ""]
 ]
 
-def possible(y,x,n)
-for y in range(1, 4):
-    for x in range(0, 4):
+def possible(y,x,n):
+    for i in range(0, 4):
+        if board[y][i] == n:
+            return False
+    for i in range(0, 4):
+        if board[i][x] == n:
+            return False
+    return True
+
+print(possible(1,0,"A"))
 
 
-    print(np.matrix(board))
+print(np.matrix(board))
 
