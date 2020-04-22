e12284a2e72c806f0921151a8959c4ccadef31cf
Rodri.inc <faguezp@gmail.com>
2020-04-22 19:01:51 -0300

    Initial commit

---
+++ sudok.py
@@ -0,0 +1,16 @@
+import numpy as np
+r = ["A", "B", "C", "D"]
+board = [
+    ["A", "B", "C", "D"],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""]
+]
+
+def possible(y,x,n)
+for y in range(1, 4):
+    for x in range(0, 4):
+
+
+    print(np.matrix(board))
+
